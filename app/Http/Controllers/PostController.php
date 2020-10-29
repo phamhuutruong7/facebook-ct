ad6e934b63442225925232862e4588e1458cec28
Pham Huu Truong <phamhuu.truong.7@gmail.com>
2020-10-29 17:17:51 +0100

    Refactor Post & User with Resources
    using Resource to control the response data.
    with php artisan make:resource [model].
    that will make a app\Http\Resources Folder for each model.
    This resource class receive a input as $request. and return an array. of data  which point to itself.
    The repair in Post model to repair the relation between User and Post.

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Resources\Post as PostResource;
 use App\Post;
 
 class PostController extends Controller
@@ -14,18 +15,7 @@ class PostController extends Controller
         ]);
 
         $post = request()->user()->posts()->create($data['data']['attributes']);
-        return response([
-            'data' => [
-                'type' => 'posts',
-                'post_id' => $post->id,
-                'attributes' => [
-                    'body' => $post->body,
-                ]
-            ],
-            'links' => [
-                'self' => url('/posts/'.$post->id),
-            ]
-        ], 201);
+        return new PostResource($post);
     }
 
 
